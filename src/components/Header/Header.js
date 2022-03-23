bf12870a7158ba418a6375066243e9a441fd93f3
abulbashar98 <abulbashar98116@gmail.com>
2022-03-23 12:48:03 +0600

    Header-Component-ready

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,18 @@
-
-
 import React from 'react';
+import logo from '../../images/Logo.svg'
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is a Header</h1>
-        </div>
+        <nav>
+            <img src={logo} alt="" />
+            <div>
+                <a href="/Home">Home</a>
+                <a href="/Shop">Shop</a>
+                <a href="/About">About</a>
+                <a href="/Contact">Contact</a>
+            </div>
+        </nav>
     );
 };
 
